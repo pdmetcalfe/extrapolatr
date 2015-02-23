d01e628a45b3ffe0733f35d9a85e2a0f5ac1ec51
Paul Metcalfe <paul.metcalfe@gmail.com>
2015-02-23 19:29:15 +0000

    Further doc tweaks

--- R/extrapolatr-package.R
+++ R/extrapolatr-package.R
@@ -1,4 +1,9 @@
 ##' Sequence extrapolation
+##'
+##' This package provides tools for convergence acceleration of vector
+##' sequences.  This is perhaps unusual for R, but this is a handy
+##' tool to have at one's disposal when doing, for instance,
+##' expectation maximization.
 ##' 
 ##' @docType package
 ##' @useDynLib extrapolatr
