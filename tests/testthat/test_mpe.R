381c0f0a73a155a0dc49201487f5befe9e83166b
Paul Metcalfe <paul.metcalfe@gmail.com>
2015-02-26 14:15:40 +0000

    Add in tests for MPE

---
+++ tests/testthat/test_mpe.R
@@ -0,0 +1,37 @@
+context("MPE")
+
+test_that("rre gives good answers (1)", {
+    ind <- seq_len(5) - 1
+    xs <- 0.99^ind
+    expect_equal(mpe(matrix(2 + xs, nrow=1)), 2)
+})
+
+test_that("rre gives good answers (2)", {
+    ind <- seq_len(5) - 1
+    xs <- 0.99^ind
+    ys <- 0.5^ind
+    zs <- 0.25^ind
+    expect_equal(mpe(rbind(1 + xs,
+                           2 + ys,
+                           3 + zs)), c(1, 2, 3))
+})
+
+test_that("rre gives good answers (3)", {
+    ind <- seq_len(5) - 1
+    xs <- 0.99^ind
+    ys <- (-0.5)^ind
+    zs <- 0.25^ind
+    expect_equal(rre(rbind(1 + xs,
+                           2 + ys,
+                           3 + zs)), c(1, 2, 3))
+})
+
+test_that("rre gives good answers (4)", {
+    ind <- seq_len(5) - 1
+    xs <- 0.99^ind
+    ys <- (-2)^ind
+    zs <- 0.25^ind
+    expect_equal(mpe(rbind(1 + xs + ys,
+                           2 + ys + zs,
+                           3 + zs)), c(1, 2, 3))
+})
